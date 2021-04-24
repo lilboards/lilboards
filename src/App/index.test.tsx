597bb41c5ba60236a2eae7e3ecda220c83f68048
Mark <mark@remarkablemark.org>
2021-04-24 13:49:09 -0400

    test(App): add snapshot tests

--- src/App/index.test.tsx
+++ src/App/index.test.tsx
@@ -1,7 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from '.';
 
-test('renders learn react link', () => {
+it('renders correctly', () => {
+  const { container } = render(<App />);
+  expect(container).toMatchSnapshot();
+});
+
+it('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
