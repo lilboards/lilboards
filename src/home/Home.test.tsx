b7a7b10ebe2c114fb0de210c1aba8d8a2c547493
Mark <mark@remarkablemark.org>
2021-04-24 16:53:40 -0400

    feat(home): render Header in Home

--- src/home/Home.test.tsx
+++ src/home/Home.test.tsx
@@ -1,13 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Home from './Home';
 
-it('renders correctly', () => {
-  const { container } = render(<Home />);
-  expect(container).toMatchSnapshot();
-});
-
-it('renders home', () => {
+it('renders header', () => {
   render(<Home />);
-  const homeElement = screen.getByText(/home/i);
-  expect(homeElement).toBeInTheDocument();
+  expect(screen.getByRole('banner')).toBeInTheDocument();
 });
--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,3 +1,9 @@
+import Header from '../common/Header';
+
 export default function Home() {
-  return <>Home</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
