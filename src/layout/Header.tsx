def06e800d2856cc749dfa94d53d98faa21da166
Mark <mark@remarkablemark.org>
2021-04-25 00:06:19 -0400

    feat(layout): change heading to link in Header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,15 +1,18 @@
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Link as RouterLink } from '@reach/router';
 
 export default function Header() {
   return (
     <AppBar position="static">
       <Toolbar>
         <Box flexGrow={1}>
-          <Typography variant="h6">Lilboards</Typography>
+          <Link color="inherit" component={RouterLink} to="/" variant="h6">
+            Lilboards
+          </Link>
         </Box>
 
         <Button color="inherit" variant="outlined">
