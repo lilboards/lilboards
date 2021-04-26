6bcdd534af2abb732a7daf80af60e320dd990666
Mark <mark@remarkablemark.org>
2021-04-26 01:31:34 -0400

    feat(firebase): get reference to database service

---
+++ src/firebase/database.ts
@@ -0,0 +1,15 @@
+import 'firebase/database';
+import firebaseApp from './app';
+import { isDevelopment, isLocalhost } from '../config';
+
+const firebaseDatabase = firebaseApp.database();
+
+/* istanbul ignore next */
+if (isDevelopment && isLocalhost) {
+  const databaseUrl = new URL(
+    process.env.REACT_APP_FIREBASE_DATABASE_URL || ''
+  );
+  firebaseDatabase.useEmulator(databaseUrl.hostname, Number(databaseUrl.port));
+}
+
+export default firebaseDatabase;
