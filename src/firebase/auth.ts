b942870e02845d9b74856120ed8182daed90979b
Mark <mark@remarkablemark.org>
2021-04-26 01:31:09 -0400

    feat(firebase): get reference to auth service

---
+++ src/firebase/auth.ts
@@ -0,0 +1,13 @@
+import 'firebase/auth';
+import firebaseApp from './app';
+import { isDevelopment, isLocalhost } from '../config';
+
+const firebaseAuth = firebaseApp.auth();
+
+/* istanbul ignore next */
+if (isDevelopment && isLocalhost) {
+  const emulatorHost = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '';
+  firebaseAuth.useEmulator(emulatorHost);
+}
+
+export default firebaseAuth;
