9e042c0cec07236040e5bd929a0971a3de8f446a
Mark <mark@remarkablemark.org>
2021-04-26 01:17:56 -0400

    feat(config): check env and host

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+/* istanbul ignore file */
+export const isDevelopment = process.env.NODE_ENV === 'development';
+
+// eslint-disable-next-line no-restricted-globals
+export const isLocalhost = location.hostname === 'localhost';
