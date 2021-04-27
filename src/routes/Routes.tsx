d45749a7ce874c3f057efca882a7a003e2ffb1f0
Mark <mark@remarkablemark.org>
2021-04-27 02:01:23 -0400

    feat(routes): render Boards in Routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,14 @@
 import { Router } from '@reach/router';
 import Home from '../home';
 import Login from '../login';
+import Boards from '../boards';
 
 export default function Routes() {
   return (
     <Router>
       <Home path="/" />
       <Login path="/login" />
+      <Boards path="/boards" />
     </Router>
   );
 }
