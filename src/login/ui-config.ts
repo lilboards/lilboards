1670f9c6bf22fe2885b45b44ffe75c813cf76d38
Mark <mark@remarkablemark.org>
2021-05-06 22:43:08 -0400

    feat(login): update sign in auth providers to only Google and email

--- src/login/ui-config.ts
+++ src/login/ui-config.ts
@@ -9,11 +9,7 @@ const uiConfig = {
   signInFlow: 'popup',
   signInOptions: [
     auth.GoogleAuthProvider.PROVIDER_ID,
-    auth.FacebookAuthProvider.PROVIDER_ID,
-    auth.TwitterAuthProvider.PROVIDER_ID,
-    auth.GithubAuthProvider.PROVIDER_ID,
     auth.EmailAuthProvider.PROVIDER_ID,
-    auth.PhoneAuthProvider.PROVIDER_ID,
   ],
 };
 
