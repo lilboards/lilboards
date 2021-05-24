7f328f1c5e92015645c59a8e43bda47b1338a04b
Mark <mark@remarkablemark.org>
2021-05-23 23:25:11 -0400

    fix(layout): wrap Header in Container so it's aligned with Layout

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,6 +1,7 @@
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Link as RouterLink } from '@reach/router';
@@ -8,22 +9,24 @@ import { Link as RouterLink } from '@reach/router';
 export default function Header() {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Box flexGrow={1}>
-          <Link color="inherit" component={RouterLink} to="/" variant="h6">
-            Lilboards
-          </Link>
-        </Box>
+      <Container>
+        <Toolbar disableGutters>
+          <Box flexGrow={1}>
+            <Link color="inherit" component={RouterLink} to="/" variant="h6">
+              Lilboards
+            </Link>
+          </Box>
 
-        <Button
-          color="inherit"
-          component={RouterLink}
-          to="/login"
-          variant="outlined"
-        >
-          Login
-        </Button>
-      </Toolbar>
+          <Button
+            color="inherit"
+            component={RouterLink}
+            to="/login"
+            variant="outlined"
+          >
+            Login
+          </Button>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 }
