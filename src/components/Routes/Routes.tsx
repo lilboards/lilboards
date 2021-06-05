4a3412acf855dcaf8200817832963a27e3675ed3
Mark <mark@remarkablemark.org>
2021-06-05 14:09:53 -0400

    feat(components): render Login in Routes

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -2,6 +2,7 @@ import { Router } from '@reach/router';
 import Boards from '../Boards';
 import Home from '../Home';
 import Login from '../Login';
+import Logout from '../Logout';
 import NotFound from '../NotFound';
 
 export default function Routes() {
@@ -10,6 +11,7 @@ export default function Routes() {
       <NotFound default />
       <Home path="/" />
       <Login path="/login" />
+      <Logout path="/logout" />
       <Boards path="/boards" />
     </Router>
   );
