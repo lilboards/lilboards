aa1a1bf0959a002ebfcad11b0c9185e553537575
Mark <mark@remarkablemark.org>
2021-06-06 00:47:42 -0400

    chore: update description and Home copy

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,7 +5,7 @@ import Layout from '../Layout';
 export default function Home(props: RouteComponentProps) {
   return (
     <Layout>
-      <Typography>Create boards and items.</Typography>
+      <Typography>Create boards and items with Lilboards.</Typography>
     </Layout>
   );
 }
