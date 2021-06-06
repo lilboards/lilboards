c0686c28980e374b6ec10631f1b02462fc9edc50
Mark <mark@remarkablemark.org>
2021-06-05 22:18:20 -0400

    test(components): render Header with store

--- src/components/Layout/Header.test.tsx
+++ src/components/Layout/Header.test.tsx
@@ -1,19 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithStore } from '../../utils/test';
 import Header from './Header';
 
 it('renders correctly', () => {
-  const { container } = render(<Header />);
+  const { container } = renderWithStore(<Header />);
   expect(container).toMatchSnapshot();
 });
 
 it('renders header', () => {
-  render(<Header />);
+  renderWithStore(<Header />);
   const headerElement = screen.getByRole('banner');
   expect(headerElement).toBeInTheDocument();
   expect(headerElement).toHaveTextContent('Lilboards');
 });
 
 it('renders login button', () => {
-  render(<Header />);
+  renderWithStore(<Header />);
   expect(screen.getByText('Login')).toBeInTheDocument();
 });
