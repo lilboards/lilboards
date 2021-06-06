3b150c37006d930c732c32a1127a0ae949c101f6
Mark <mark@remarkablemark.org>
2021-06-06 00:53:42 -0400

    feat(components): stylize copy as heading in Boards

--- src/components/Boards/Boards.tsx
+++ src/components/Boards/Boards.tsx
@@ -1,6 +1,14 @@
+import Typography from '@material-ui/core/Typography';
 import type { RouteComponentProps } from '@reach/router';
+
 import Layout from '../Layout';
 
 export default function Boards(props: RouteComponentProps) {
-  return <Layout>Boards</Layout>;
+  return (
+    <Layout>
+      <Typography component="h1" gutterBottom variant="h4">
+        Boards
+      </Typography>
+    </Layout>
+  );
 }
