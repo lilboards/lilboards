f2fc3e1a074b7597801306fb3441f77b30819b84
Mark <mark@remarkablemark.org>
2021-06-06 20:37:43 -0400

    feat(store): add boards slice reducer to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+
+import {
+  name as boardsReducerName,
+  reducer as boardsReducer,
+} from './boardsSlice';
 import { name as userReducerName, reducer as userReducer } from './userSlice';
 
 const store = configureStore({
   reducer: {
+    [boardsReducerName]: boardsReducer,
     [userReducerName]: userReducer,
   },
 });
