2200550de8ded43374d03df6144f38c34694de56
Mark <mark@remarkablemark.org>
2021-06-06 20:38:34 -0400

    feat(actions): consolidate boards actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,8 @@
+import { actions as boardsActions } from '../store/boardsSlice';
 import { actions as userActions } from '../store/userSlice';
 
 const actions = {
+  ...boardsActions,
   ...userActions,
 };
 
