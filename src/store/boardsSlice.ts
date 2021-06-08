5b2e1209a84f3b187dd03da229f629f365a69ec6
Mark <mark@remarkablemark.org>
2021-06-08 03:00:51 -0400

    chore(store): refactor types in boardsSlice

--- src/store/boardsSlice.ts
+++ src/store/boardsSlice.ts
@@ -10,6 +10,8 @@ type Boards = {
   [id: string]: Board;
 };
 
+type Id = string;
+
 export const initialState: Boards = {};
 
 export const name = 'boards';
@@ -19,14 +21,14 @@ const slice = createSlice({
   initialState,
 
   reducers: {
-    addBoard: (state, action: PayloadAction<string>) => {
+    addBoard: (state, action: PayloadAction<Id>) => {
       const board: Board = {
         name: '',
       };
       const boardRef = boardsRef.push();
       boardRef.set(board);
 
-      const boardId = boardRef.key as string;
+      const boardId = boardRef.key as Id;
       const userId = action.payload;
       usersRef
         .child(userId)
@@ -37,17 +39,20 @@ const slice = createSlice({
       state[boardId] = board;
     },
 
-    editBoard: (state, action: PayloadAction<Board & { id: string }>) => {
-      const { payload } = action;
-      delete payload.focus;
-      const { id, ...restPayload } = payload;
-      boardsRef.child(id).update(restPayload);
-      state[id] = restPayload;
+    editBoard: (state, action: PayloadAction<Board & { id: Id }>) => {
+      const board = action.payload;
+      /* istanbul ignore next */
+      if (board.focus) {
+        delete board.focus;
+      }
+      const { id, ...restBoard } = board;
+      boardsRef.child(id).update(restBoard);
+      state[id] = restBoard;
     },
 
     deleteBoard: (
       state,
-      action: PayloadAction<{ boardId: string; userId: string }>
+      action: PayloadAction<{ boardId: Id; userId: Id }>
     ) => {
       const { boardId, userId } = action.payload;
       boardsRef.child(boardId).remove();
@@ -55,10 +60,7 @@ const slice = createSlice({
       delete state[boardId];
     },
 
-    loadBoard: (
-      state,
-      action: PayloadAction<(Board & { id: string }) | null>
-    ) => {
+    loadBoard: (state, action: PayloadAction<(Board & { id: Id }) | null>) => {
       const board = action.payload;
       if (board) {
         const { id, ...restBoard } = board;
