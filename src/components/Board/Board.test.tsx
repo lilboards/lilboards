da4aac0f6e59b5ca210c483f92a4ccdaa82bd504
Mark <mark@remarkablemark.org>
2021-06-08 22:15:53 -0400

    feat(components): replace board placeholder with name

--- src/components/Board/Board.test.tsx
+++ src/components/Board/Board.test.tsx
@@ -1,10 +1,57 @@
 import { screen } from '@testing-library/react';
-import { renderWithStore } from '../../utils/test';
+import { renderWithStore, updateStore } from '../../utils/test';
+import { boardsRef } from '../../firebase';
 import Board from './Board';
 
+jest.mock('../../firebase', () => ({
+  boardsRef: {
+    child: jest.fn(),
+    on: jest.fn(),
+    off: jest.fn(),
+  },
+}));
+
+beforeEach(() => {
+  (boardsRef.child as jest.Mock).mockReturnThis();
+});
+
+it('render null when boardId is empty', () => {
+  const { baseElement } = renderWithStore(<Board />);
+  expect(baseElement.firstChild).toBeEmptyDOMElement();
+});
+
+it('render null when there are no boards', () => {
+  const { baseElement } = renderWithStore(<Board boardId="board1" />);
+  expect(baseElement.firstChild).toBeEmptyDOMElement();
+});
+
 it('renders heading', () => {
-  renderWithStore(<Board />);
+  const { id, name } = updateStore.withBoard();
+  renderWithStore(<Board boardId={id} />);
   expect(screen.getByRole('heading', { level: 1 })).toBe(
-    screen.getByText('Board')
+    screen.getByText(name)
   );
+});
+
+describe('mount', () => {
+  beforeAll(() => {
+    const snapshot = {
+      val: () => ({
+        created: Date.now(),
+        name: 'Board Name',
+        updated: Date.now(),
+      }),
+    };
+    (boardsRef.on as jest.Mock).mockImplementationOnce(
+      (eventType, successCallback) =>
+        eventType === 'value' && successCallback(snapshot)
+    );
+    renderWithStore(<Board boardId="board1" />);
+  });
+
+  it('loads board', () => {
+    expect(screen.getByRole('heading', { level: 1 })).toBe(
+      screen.getByText('Board Name')
+    );
+  });
 });
