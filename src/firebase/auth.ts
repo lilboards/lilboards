cf345b363ea7c0b9c45c5f66c2639a5700913c87
Mark <mark@remarkablemark.org>
2021-06-14 22:58:22 -0400

    chore(firebase): ignore files from test coverage

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,10 +1,10 @@
+/* istanbul ignore file */
 import 'firebase/auth';
 import firebaseApp from './app';
 import { isDevelopment, isLocalhost } from '../config';
 
 const firebaseAuth = firebaseApp.auth();
 
-/* istanbul ignore next */
 if (isDevelopment && isLocalhost) {
   const emulatorHost = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '';
   firebaseAuth.useEmulator(emulatorHost);
