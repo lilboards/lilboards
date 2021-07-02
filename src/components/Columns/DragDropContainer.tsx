88c715dd93dff2efa968abd0dc1b7590d50c9942
Mark <mark@remarkablemark.org>
2021-07-02 15:26:51 -0400

    feat(components): reorder items on drag end in DragDropContainer

--- src/components/Columns/DragDropContainer.tsx
+++ src/components/Columns/DragDropContainer.tsx
@@ -1,14 +1,46 @@
 import { DragDropContext } from 'react-beautiful-dnd';
 
+import actions from '../../actions';
+import { useDispatch, useSelector } from '../../hooks';
+import { reorderColumns } from './reorder';
+
 import type { ReactNode } from 'react';
+import type { DropResult } from 'react-beautiful-dnd';
+import type { Id } from '../../types';
 
 type Props = {
+  boardId: Id;
   children: ReactNode;
 };
 
 export default function DragDropContainer(props: Props) {
+  const dispatch = useDispatch();
+  const columns = useSelector((state) => state.columns);
+
   /* istanbul ignore next */
-  function handleDragEnd() {}
+  function handleDragEnd(result: DropResult) {
+    const { destination, source } = result;
+
+    // dropped nowhere
+    if (!destination) {
+      return;
+    }
+
+    // did not move anywhere, can bail early
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    ) {
+      return;
+    }
+
+    dispatch(
+      actions.setColumnItemIds({
+        boardId: props.boardId,
+        columnItemIds: reorderColumns(columns, source, destination),
+      })
+    );
+  }
 
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
