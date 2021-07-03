768a88ea121ef98bd2538b6641004dff8d55e89d
Mark <mark@remarkablemark.org>
2021-07-03 14:30:05 -0400

    refactor(components): update getBackgroundColor return values

--- src/components/Items/styles.test.ts
+++ src/components/Items/styles.test.ts
@@ -1,19 +1,19 @@
 import { getBackgroundColor } from './styles';
 
 describe('getBackgroundColor', () => {
-  it('returns bgcolor when dragging over', () => {
+  it('returns hover color when dragging over', () => {
     const snapshot = {
       isDraggingOver: true,
       isUsingPlaceholder: true,
     };
-    expect(getBackgroundColor(snapshot)).toBe('grey.200');
+    expect(getBackgroundColor(snapshot)).toBe('action.hover');
   });
 
-  it('returns null when not dragging over', () => {
+  it('returns no color when not dragging over', () => {
     const snapshot = {
       isDraggingOver: false,
       isUsingPlaceholder: false,
     };
-    expect(getBackgroundColor(snapshot)).toBe(null);
+    expect(getBackgroundColor(snapshot)).toBe('');
   });
 });
--- src/components/Items/styles.ts
+++ src/components/Items/styles.ts
@@ -1,5 +1,9 @@
 import type { DroppableStateSnapshot } from 'react-beautiful-dnd';
 
 // https://github.com/atlassian/react-beautiful-dnd/blob/master/docs/api/droppable.md#recommended--home-list-styling
-export const getBackgroundColor = (snapshot: DroppableStateSnapshot) =>
-  snapshot.isDraggingOver ? 'grey.200' : null;
+export const getBackgroundColor = (snapshot: DroppableStateSnapshot) => {
+  if (snapshot.isDraggingOver) {
+    return 'action.hover';
+  }
+  return ''; // 'background.default'
+};
