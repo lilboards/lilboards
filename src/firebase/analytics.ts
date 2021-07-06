ba28cf7d4b78448fcac827e125dca93ea9da8878
Mark <mark@remarkablemark.org>
2021-07-05 21:01:49 -0400

    feat: set firebase analytics user id in userSlice reducer setUser

--- src/firebase/analytics.ts
+++ src/firebase/analytics.ts
@@ -1,13 +1,22 @@
 /* istanbul ignore file */
 import 'firebase/analytics';
 
+import type firebase from 'firebase';
+
 import { firebaseApp } from './app';
 import { isProduction } from '../config';
+import { noop } from '../utils';
 
-let firebaseAnalytics;
+type FirebaseAnalytics = ReturnType<typeof firebase.analytics>;
+
+let firebaseAnalytics: FirebaseAnalytics;
 
 if (isProduction) {
   firebaseAnalytics = firebaseApp.analytics();
+} else {
+  firebaseAnalytics = {
+    setUserId: noop,
+  } as unknown as FirebaseAnalytics;
 }
 
 export { firebaseAnalytics };
