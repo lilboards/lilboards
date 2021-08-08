270ba0eb701ebe9640ef72de540dde219ff9a0f9
Mark <mark@remarkablemark.org>
2021-08-08 17:50:14 -0400

    test(components): replace renderWithStore with render

--- src/components/NotFound/NotFound.test.tsx
+++ src/components/NotFound/NotFound.test.tsx
@@ -1,8 +1,21 @@
-import { screen } from '@testing-library/react';
-import { renderWithStore } from '../../utils/test';
+import { render, screen } from '@testing-library/react';
 import NotFound from './NotFound';
 
 it('renders not found', () => {
-  renderWithStore(<NotFound />);
-  expect(screen.getByText('Not Found')).toBeInTheDocument();
+  render(<NotFound />);
+  expect(
+    screen.getByRole('heading', {
+      level: 1,
+      name: 'Not Found',
+    })
+  ).toBeInTheDocument();
+});
+
+it('renders home link', () => {
+  render(<NotFound />);
+  expect(
+    screen.getByRole('link', {
+      name: 'home',
+    })
+  ).toHaveAttribute('href', '/');
 });
