576c45924816e58bf1f93640686bdd360126c5ec
Mark <mark@remarkablemark.org>
2021-08-09 00:06:16 -0400

    feat(components): add Snackbar

---
+++ src/components/Snackbar/Snackbar.test.tsx
@@ -0,0 +1,31 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import Snackbar from './Snackbar';
+
+const message = 'message';
+
+it('does not render snackbar when props.open=false', () => {
+  render(<Snackbar message={message} />);
+  expect(screen.queryByText(message)).not.toBeInTheDocument();
+});
+
+it('renders snackbar when props.open=true', () => {
+  render(<Snackbar message={message} open />);
+  expect(screen.getByText(message)).toBeInTheDocument();
+});
+
+it('closes snackbar when close icon button is clicked', async () => {
+  render(<Snackbar lastOpened={Date.now()} message={message} />);
+  fireEvent.click(screen.getByLabelText('Close'));
+  await waitFor(() => {
+    expect(screen.queryByText(message)).not.toBeInTheDocument();
+  });
+});
+
+it('auto hides snackbar', async () => {
+  render(
+    <Snackbar autoHideDuration={0} lastOpened={Date.now()} message={message} />
+  );
+  await waitFor(() => {
+    expect(screen.queryByText(message)).not.toBeInTheDocument();
+  });
+});
