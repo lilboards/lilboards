196d9945d1fa4c3f9a184c4c713bc1ec89027f92
Mark <mark@remarkablemark.org>
2021-08-17 00:10:46 -0400

    fix(components): change card bgcolor when dragging over combine

--- src/components/Items/DraggableItem.tsx
+++ src/components/Items/DraggableItem.tsx
@@ -1,18 +1,22 @@
+import { useTheme } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import { Draggable } from 'react-beautiful-dnd';
 
 import Item from '../Item';
+import { getDraggableCardStyle } from './styles';
 
 import type { Id } from '../../types';
 
-type Props = {
+interface Props {
   boardId: Id;
   columnId: Id;
   itemId: Id;
   itemIndex: number;
-};
+}
 
 export default function DraggableItem(props: Props) {
+  const theme = useTheme();
+
   return (
     <Draggable draggableId={props.itemId} index={props.itemIndex}>
       {(draggableProvided, draggableSnapshot) => (
@@ -24,6 +28,7 @@ export default function DraggableItem(props: Props) {
           <Box paddingBottom={2}>
             <Item
               boardId={props.boardId}
+              cardStyle={getDraggableCardStyle(draggableSnapshot, theme)}
               columnId={props.columnId}
               itemId={props.itemId}
             />
