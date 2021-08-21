1f50fcd10b9e26c089a678c21e2e67f882f29bc0
Mark <mark@remarkablemark.org>
2021-08-21 17:15:37 -0400

    feat(components): add GitHub, remarkablemark, & version links in Home

--- src/components/Home/Home.test.tsx
+++ src/components/Home/Home.test.tsx
@@ -7,3 +7,28 @@ it('renders home', () => {
     screen.getByText('Create boards, columns, and items.')
   ).toBeInTheDocument();
 });
+
+it('renders GitHub link', () => {
+  render(<Home />);
+  expect(screen.getByText('open source')).toHaveAttribute(
+    'href',
+    'https://b.remarkabl.org/lilboards'
+  );
+});
+
+it('renders remarkablemark link', () => {
+  render(<Home />);
+  expect(screen.getByText('remarkablemark')).toHaveAttribute(
+    'href',
+    'https://b.remarkabl.org/mark'
+  );
+});
+
+it('renders version link', () => {
+  const version = process.env.REACT_APP_PROJECT_VERSION as string;
+  render(<Home />);
+  expect(screen.getByText(version)).toHaveAttribute(
+    'href',
+    `https://github.com/lilboards/lilboards/releases/tag/v${version}`
+  );
+});
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,42 @@
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
 import type { RouteComponentProps } from '@reach/router';
 
+const version = process.env.REACT_APP_PROJECT_VERSION;
+
 export default function Home(props: RouteComponentProps) {
   return (
     <>
-      <Typography>Create boards, columns, and items.</Typography>
+      <Typography paragraph>Create boards, columns, and items.</Typography>
+
+      <Typography paragraph>
+        Free and{' '}
+        <Link
+          href="https://b.remarkabl.org/lilboards"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open source
+        </Link>
+        . Built by{' '}
+        <Link
+          href="https://b.remarkabl.org/mark"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          remarkablemark
+        </Link>
+        . Version{' '}
+        <Link
+          href={`https://github.com/lilboards/lilboards/releases/tag/v${version}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {version}
+        </Link>
+        .
+      </Typography>
     </>
   );
 }
