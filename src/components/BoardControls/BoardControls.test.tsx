b59fffdae7639614be934e992da7c4d0ae1f9ac3
Mark <mark@remarkablemark.org>
2021-08-31 18:46:11 -0400

    test(components): make sure Timer is rendered in BoardControls

--- src/components/BoardControls/BoardControls.test.tsx
+++ src/components/BoardControls/BoardControls.test.tsx
@@ -24,6 +24,13 @@ it('renders "Add column" button', () => {
   ).toBeInTheDocument();
 });
 
+it('renders "Timer" input and "Start" button', () => {
+  const board = updateStore.withBoard();
+  renderWithContext(<BoardControls boardId={board.id} />);
+  expect(screen.getByLabelText('Timer in minutes')).toBeInTheDocument();
+  expect(screen.getByLabelText('Start timer')).toBeInTheDocument();
+});
+
 it('renders "Present" switch', () => {
   const board = updateStore.withBoard();
   updateStore.withUser();
