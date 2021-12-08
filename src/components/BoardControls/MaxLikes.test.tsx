8464e542d2c43daeb690d43a1b4c9ce602e8a45d
Mark <mark@remarkablemark.org>
2021-12-07 22:16:33 -0500

    feat(components): add MaxLikes

---
+++ src/components/BoardControls/MaxLikes.test.tsx
@@ -0,0 +1,64 @@
+import { fireEvent, screen } from '@testing-library/react';
+
+import { BOARD_TEST_ID as boardId } from '../../constants/test';
+import { logEvent } from '../../firebase';
+import { DEFAULT_MAX_LIKES } from '../../hooks';
+import { renderWithContext, updateStore } from '../../utils/test';
+import MaxLikes from './MaxLikes';
+
+jest.mock('../../firebase', () => ({
+  logEvent: jest.fn(),
+}));
+
+it('renders default max likes value', () => {
+  renderWithContext(<MaxLikes boardId={boardId} />);
+  expect(screen.getByRole('spinbutton', { name: 'Max Likes' })).toHaveValue(
+    DEFAULT_MAX_LIKES
+  );
+});
+
+describe('when user cannot edit', () => {
+  beforeEach(() => {
+    renderWithContext(<MaxLikes boardId={boardId} />);
+  });
+
+  it('renders disabled input', () => {
+    expect(screen.getByLabelText('Max Likes')).toBeDisabled();
+  });
+
+  it('does not change max likes value', () => {
+    const input = screen.getByLabelText('Max Likes');
+    const event = { target: { value: DEFAULT_MAX_LIKES + 1 } };
+    fireEvent.change(input, event);
+    expect(input).toHaveValue(DEFAULT_MAX_LIKES);
+    expect(logEvent).not.toBeCalled();
+  });
+});
+
+describe('when user can edit', () => {
+  let board: ReturnType<typeof updateStore.withBoard>;
+
+  beforeEach(() => {
+    board = updateStore.withBoard();
+    updateStore.withUser();
+    renderWithContext(<MaxLikes boardId={board.id} />);
+  });
+
+  it('changes max likes value', () => {
+    const input = screen.getByLabelText('Max Likes');
+    const event = { target: { value: '0' } };
+    fireEvent.change(input, event);
+    expect(input).toHaveValue(Number(event.target.value));
+  });
+
+  it('logs event', () => {
+    const input = screen.getByLabelText('Max Likes');
+    const event = { target: { value: '3.14' } };
+    fireEvent.change(input, event);
+    expect(logEvent).toBeCalledTimes(1);
+    expect(logEvent).toBeCalledWith('max_likes', {
+      boardId: board.id,
+      maxLikes: 3,
+    });
+  });
+});
