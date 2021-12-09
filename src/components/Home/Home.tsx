6c097fef6ea133236c47ff8a0044561e07878606
Mark <mark@remarkablemark.org>
2021-12-08 23:54:20 -0500

    feat(components): render Hero and Features in Home; modularize Footer

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,41 +1,15 @@
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
 import type { RouteComponentProps } from '@reach/router';
 
-const version = process.env.REACT_APP_PROJECT_VERSION;
+import Features from './Features';
+import Footer from './Footer';
+import Hero from './Hero';
 
 export default function Home(props: RouteComponentProps) {
   return (
     <>
-      <Typography paragraph>Create boards, columns, and items.</Typography>
-
-      <Typography paragraph>
-        Free and{' '}
-        <Link
-          href="https://b.remarkabl.org/lilboards"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open source
-        </Link>
-        . Built by{' '}
-        <Link
-          href="https://b.remarkabl.org/mark"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          remarkablemark
-        </Link>
-        . Version{' '}
-        <Link
-          href={`https://github.com/lilboards/lilboards/releases/tag/v${version}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {version}
-        </Link>
-        .
-      </Typography>
+      <Hero />
+      <Features />
+      <Footer />
     </>
   );
 }
