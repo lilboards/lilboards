7ac6b89310bb0f72a72cfa61b3a63e8c69529aca
Mark <mark@remarkablemark.org>
2022-05-05 22:38:25 -0400

    feat(Board): add icon button to copy board link to clipboard

--- src/components/Board/BoardName.tsx
+++ src/components/Board/BoardName.tsx
@@ -1,4 +1,9 @@
+import LinkIcon from '@mui/icons-material/Link';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+
+import { logEvent } from '../../firebase';
 
 interface Props {
   name?: string;
@@ -11,7 +16,19 @@ export default function BoardName(props: Props) {
 
   return (
     <Typography component="h1" gutterBottom variant="h4">
-      {props.name}
+      {props.name + ' '}
+
+      <Tooltip arrow placement="right" title="Copy link to board">
+        <IconButton color="info" onClick={copyBoardLinkToClipboard}>
+          <LinkIcon />
+        </IconButton>
+      </Tooltip>
     </Typography>
   );
+}
+
+async function copyBoardLinkToClipboard() {
+  const boardLink = window.location.origin + window.location.pathname;
+  await navigator.clipboard.writeText(boardLink);
+  logEvent('copy', { link: boardLink });
 }
