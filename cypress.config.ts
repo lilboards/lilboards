ddf61efaadcec853cfffed8c7ac9231af7dbe9e5
Mark <mark@remarkablemark.org>
2022-12-30 23:14:25 -0500

    test(cypress): update config to process feature files

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'cypress';
+import createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'http://localhost:3000',
+    specPattern: '**/*.feature',
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        'file:preprocessor',
+        createBundler({ plugins: [createEsbuildPlugin(config)] })
+      );
+      return config;
     },
   },
 });
