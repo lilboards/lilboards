a7ccbc94aff4715151110c2ce1f96181209a2b1c
Mark <mark@remarkablemark.org>
2023-01-01 17:04:57 -0500

    feat(Item): add item text to LikeButton label

--- src/components/Item/LikeButton.tsx
+++ src/components/Item/LikeButton.tsx
@@ -15,15 +15,16 @@ interface Props {
 export default function LikeButton(props: Props) {
   const dispatch = useDispatch();
   const userId = useSelector((state) => state.user.id);
+  const itemText = useSelector(
+    (state) => state.items[props.itemId]?.text || ''
+  );
   const likes = useSelector((state) => state.likes.items[props.itemId] || {});
   const isLikedByUser = likes[userId];
   const totalLikes = useSelector((state) =>
-    Object.values(state.likes.items).reduce((accumulator, item) => {
-      if (item[userId]) {
-        accumulator++;
-      }
-      return accumulator;
-    }, 0)
+    Object.values(state.likes.items).reduce(
+      (sum, item) => sum + Number(Boolean(item[userId])),
+      0
+    )
   );
   const maxLikes = useMaxLikes(props.boardId);
   const hideLikes = useSelector((state) => state.user.hideLikes);
@@ -46,7 +47,7 @@ export default function LikeButton(props: Props) {
   return (
     <IconButton
       size="small"
-      aria-label={`${isLikedByUser ? 'Unlike' : 'Like'} item`}
+      aria-label={`${isLikedByUser ? 'Unlike' : 'Like'} item "${itemText}"`}
       onClick={handleClick}
     >
       {isLikedByUser && !hideLikes ? (
