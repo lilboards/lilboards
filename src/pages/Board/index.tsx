bfa058eb51fc010f687dfd22e032672560faf540
Mark <mark@remarkablemark.org>
2023-03-08 21:07:19 -0500

    feat(pages): render Connection in Board and Boards

--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -2,11 +2,13 @@ import CircularProgress from '@mui/material/CircularProgress';
 import { lazy, Suspense } from 'react';
 
 const Board = lazy(() => import('./Board'));
+const Connection = lazy(() => import('../../components/Connection'));
 
 export default function BoardLoader() {
   return (
     <Suspense fallback={<CircularProgress />}>
       <Board />
+      <Connection />
     </Suspense>
   );
 }
--- src/pages/Boards/index.tsx
+++ src/pages/Boards/index.tsx
@@ -2,11 +2,13 @@ import CircularProgress from '@mui/material/CircularProgress';
 import { lazy, Suspense } from 'react';
 
 const Boards = lazy(() => import('./Boards'));
+const Connection = lazy(() => import('../../components/Connection'));
 
 export default function BoardsLoader() {
   return (
     <Suspense fallback={<CircularProgress />}>
       <Boards />
+      <Connection />
     </Suspense>
   );
 }
