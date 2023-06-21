c0a409f5b3111468a16c21f1a2011cd1c595bc90
Mark <mark@remarkablemark.org>
2023-06-21 00:16:50 -0400

    perf(Boards): use createSelector in BoardCards
    ```
    Selector unknown returned a different result when called with the same parameters. This can lead to unnecessary rerenders.
    Selectors that return a new reference (such as an object or an array) should be memoized:
    https://redux.js.org/usage/deriving-data-selectors#optimizing-selectors-with-memoization
    ```

--- src/pages/Boards/BoardCards.tsx
+++ src/pages/Boards/BoardCards.tsx
@@ -1,20 +1,26 @@
 import Grid from '@mui/material/Grid';
+import { createSelector } from '@reduxjs/toolkit';
 
 import { useSelector } from '../../hooks';
-import { SortBy, SortOrder } from '../../types';
+import { RootState, SortBy, SortOrder } from '../../types';
 import { sort } from '../../utils';
 import BoardCard from './BoardCard';
 
-export default function BoardCards() {
-  const boardIds = useSelector((state) => {
-    const boards = Object.keys(state.boards).map((id) => ({
-      ...state.boards[id],
+const selectBoardIds = createSelector(
+  (state: RootState) => state.boards,
+  (boards) => {
+    const boardsWithId = Object.keys(boards).map((id) => ({
+      ...boards[id],
       id,
     }));
-    return sort(boards, SortBy.createdAt, SortOrder.Descending).map(
+    return sort(boardsWithId, SortBy.createdAt, SortOrder.Descending).map(
       (board) => board.id
     );
-  });
+  }
+);
+
+export default function BoardCards() {
+  const boardIds = useSelector(selectBoardIds);
 
   if (!boardIds.length) {
     return null;
