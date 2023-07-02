d0b2a7f314c088d5dc6b2a9bdd7df881dcf85c57
Mark <mark@remarkablemark.org>
2023-07-02 13:05:02 -0400

    test(BoardControls): refactor to snapshots for utils transform

--- src/components/BoardControls/utils/transform.test.ts
+++ src/components/BoardControls/utils/transform.test.ts
@@ -1,72 +1,91 @@
+import type { Column, Item } from '../../../types';
 import { transformToMarkdown, transformToRows } from './transform';
 
-describe('transformToRows', () => {
-  it('transforms empty columns to empty items', () => {
-    expect(transformToRows({}, {})).toEqual([]);
+const column: Column = {
+  createdAt: Date.now(),
+  createdBy: 'user1',
+  name: '',
+} as const;
+
+const item: Item = {
+  createdAt: Date.now(),
+  createdBy: 'user1',
+  text: '',
+} as const;
+
+describe.each([transformToRows, transformToMarkdown])('%p', (transform) => {
+  it('transforms empty columns', () => {
+    expect(transform({}, {})).toMatchSnapshot();
+  });
+
+  it('transforms columns with no name', () => {
+    expect(
+      transform(
+        {
+          column1: column,
+          column2: column,
+        },
+        {}
+      )
+    ).toMatchSnapshot();
   });
 
   it('transforms column with no items', () => {
     expect(
-      transformToRows(
+      transform(
         {
           column1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            itemIds: [],
+            ...column,
             name: 'Column Name',
           },
         },
         {}
       )
-    ).toEqual([['Column Name']]);
+    ).toMatchSnapshot();
   });
 
   it('transforms columns with no items', () => {
     expect(
-      transformToRows(
+      transform(
         {
           column1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...column,
             name: 'Column Name 1',
           },
           column2: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...column,
             itemIds: [],
             name: 'Column Name 2',
           },
         },
         {}
       )
-    ).toEqual([['Column Name 1', 'Column Name 2']]);
+    ).toMatchSnapshot();
   });
 
   it('transforms column with item', () => {
     expect(
-      transformToRows(
+      transform(
         {
           column1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...column,
             itemIds: ['item1'],
             name: 'Column Name',
           },
         },
         {
           item1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...item,
             text: 'Item 1',
           },
         }
       )
-    ).toEqual([['Column Name'], ['Item 1']]);
+    ).toMatchSnapshot();
   });
 
   it('transforms column with items', () => {
     expect(
-      transformToRows(
+      transform(
         {
           column1: {
             createdAt: Date.now(),
@@ -77,242 +96,68 @@ describe('transformToRows', () => {
         },
         {
           item1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...item,
             text: 'Item 1',
           },
           item2: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...item,
             text: 'Item 2',
           },
         }
       )
-    ).toEqual([['Column Name'], ['Item 1'], ['Item 2']]);
+    ).toMatchSnapshot();
   });
 
   it('transforms columns with items', () => {
     expect(
-      transformToRows(
+      transform(
         {
           column1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...column,
             itemIds: ['item1'],
             name: 'Column 1',
           },
           column2: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...column,
             itemIds: ['item2', 'item3'],
             name: 'Column 2',
           },
         },
         {
           item1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...item,
             text: 'Item 1',
           },
           item2: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...item,
             text: 'Item 2',
           },
           item3: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...item,
             text: 'Item 3',
           },
         }
       )
-    ).toEqual([
-      ['Column 1', 'Column 2'],
-      ['Item 1', 'Item 2'],
-      [undefined, 'Item 3'],
-    ]);
-  });
-});
-
-describe('transformToMarkdown', () => {
-  it('transforms empty columns to empty string', () => {
-    expect(transformToMarkdown({}, {})).toBe('');
-  });
-
-  it('transforms column with no items', () => {
-    expect(
-      transformToMarkdown(
-        {
-          column1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            itemIds: [],
-            name: 'Column Name',
-          },
-        },
-        {}
-      )
-    ).toMatchInlineSnapshot(`
-      "| Column Name |
-      | --- |
-      "
-    `);
-  });
-
-  it('transforms columns with no items', () => {
-    expect(
-      transformToMarkdown(
-        {
-          column1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            name: 'Column Name 1',
-          },
-          column2: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            itemIds: [],
-            name: 'Column Name 2',
-          },
-        },
-        {}
-      )
-    ).toMatchInlineSnapshot(`
-      "| Column Name 1 | Column Name 2 |
-      | --- | --- |
-      "
-    `);
-  });
-
-  it('transforms column with item', () => {
-    expect(
-      transformToMarkdown(
-        {
-          column1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            itemIds: ['item1'],
-            name: 'Column Name',
-          },
-        },
-        {
-          item1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            text: 'Item 1',
-          },
-        }
-      )
-    ).toMatchInlineSnapshot(`
-      "| Column Name |
-      | --- |
-      | Item 1 |
-      "
-    `);
+    ).toMatchSnapshot();
   });
 
   it('transforms column with item with newlines', () => {
     expect(
-      transformToMarkdown(
+      transform(
         {
           column1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...column,
             itemIds: ['item1'],
             name: 'Column Name',
           },
         },
-
         {
           item1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
+            ...item,
             text: 'Item\n\n1',
           },
         }
       )
-    ).toMatchInlineSnapshot(`
-      "| Column Name |
-      | --- |
-      | Item<br><br>1 |
-      "
-    `);
-  });
-
-  it('transforms column with items', () => {
-    expect(
-      transformToMarkdown(
-        {
-          column1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            itemIds: ['item1', 'item2'],
-            name: 'Column Name',
-          },
-        },
-        {
-          item1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            text: 'Item 1',
-          },
-          item2: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            text: 'Item 2',
-          },
-        }
-      )
-    ).toMatchInlineSnapshot(`
-      "| Column Name |
-      | --- |
-      | Item 1 |
-      | Item 2 |
-      "
-    `);
-  });
-
-  it('transforms columns with items', () => {
-    expect(
-      transformToMarkdown(
-        {
-          column1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            itemIds: ['item1'],
-            name: 'Column 1',
-          },
-          column2: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            itemIds: ['item2', 'item3'],
-            name: 'Column 2',
-          },
-        },
-        {
-          item1: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            text: 'Item 1',
-          },
-          item2: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            text: 'Item 2',
-          },
-          item3: {
-            createdAt: Date.now(),
-            createdBy: 'user1',
-            text: 'Item 3',
-          },
-        }
-      )
-    ).toMatchInlineSnapshot(`
-      "| Column 1 | Column 2 |
-      | --- | --- |
-      | Item 1 | Item 2 |
-      |  | Item 3 |
-      "
-    `);
+    ).toMatchSnapshot();
   });
 });
