88114ae97e7ac63918ad550d0b9e29c8dc80356d
Mark <mark@remarkablemark.org>
2023-09-17 19:59:10 -0400

    build(cypress): update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'cypress';
-import createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
-import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
+import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/e2e/**/*.feature',
+    specPattern: '**/*.feature',
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
       on(
@@ -16,7 +16,6 @@ export default defineConfig({
       return config;
     },
   },
-
   retries: {
     runMode: 3,
   },
