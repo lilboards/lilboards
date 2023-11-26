0576a369da8b4b54988acde849cca30cf34ccfe9
Mark <mark@remarkablemark.org>
2023-11-26 18:17:13 -0500

    test(firebase): disable auth emulator warnings
    https://github.com/firebase/firebase-tools/issues/2773#issuecomment-744497705

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -15,11 +15,11 @@ export const firebaseAuth = getAuth(firebaseApp);
 
 if (isDevelopment && isLocalhost) {
   /**
-   * {@link https://firebase.google.com/docs/emulator-suite/connect_auth#web-version-9}
+   * @see https://firebase.google.com/docs/emulator-suite/connect_auth#web-modular-api
    */
-  const emulatorHost = 'http://localhost:9099';
+  const emulatorHost = 'http://127.0.0.1:9099';
   const { connectAuthEmulator } = require('firebase/auth');
-  connectAuthEmulator(firebaseAuth, emulatorHost);
+  connectAuthEmulator(firebaseAuth, emulatorHost, { disableWarnings: true });
 }
 
 /**
