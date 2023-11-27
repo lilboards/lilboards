fc86dc59adc2c4f1dcaf827364bf9594a9dd2f97
Mark <mark@remarkablemark.org>
2023-11-26 20:39:33 -0500

    build(vite): add plugin vite-plugin-commonjs to fix require is not defined

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
 });
