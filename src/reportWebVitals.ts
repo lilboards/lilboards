3de5dd330764e85eaa29611f1a2f6e70afbaf1c4
Mark <mark@remarkablemark.org>
2025-05-08 23:36:14 -0400

    refactor: replace `onFID` with `onINP` in reportWebVitals.ts
    https://github.com/GoogleChrome/web-vitals/blob/main/docs/upgrading-to-v5.md

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -4,9 +4,9 @@ import { ReportCallback } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportCallback) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+    import('web-vitals').then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
       onCLS(onPerfEntry);
-      onFID(onPerfEntry);
+      onINP(onPerfEntry);
       onFCP(onPerfEntry);
       onLCP(onPerfEntry);
       onTTFB(onPerfEntry);
