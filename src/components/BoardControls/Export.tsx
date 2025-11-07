389437771e29262c1e730502d0e58e8620326da5
Mark <mark@remarkablemark.org>
2025-11-07 18:45:21 -0500

    feat(components): render SplitButton in Export

--- src/components/BoardControls/Export.tsx
+++ src/components/BoardControls/Export.tsx
@@ -5,11 +5,14 @@ import type { SxProps } from '@mui/system';
 import { logEvent } from 'src/firebase';
 import { useSelector } from 'src/hooks';
 
+import SplitButton from '../SplitButton';
 import { transformToMarkdown } from './utils';
 
 interface Props {
   sx?: SxProps;
 }
+
+const options = ['Copy as Markdown', 'Copy as CSV'];
 
 export default function Export(props: Props) {
   const columns = useSelector((state) => state.columns);
@@ -22,10 +25,14 @@ export default function Export(props: Props) {
   }
 
   return (
-    <Tooltip arrow sx={props.sx} title="Copy board as Markdown">
-      <IconButton color="info" onClick={copyBoardMarkdownToClipboard}>
-        <FileDownloadIcon />
-      </IconButton>
-    </Tooltip>
+    <>
+      <SplitButton options={options} />
+
+      <Tooltip arrow sx={props.sx} title="Copy board as Markdown">
+        <IconButton color="info" onClick={copyBoardMarkdownToClipboard}>
+          <FileDownloadIcon />
+        </IconButton>
+      </Tooltip>
+    </>
   );
 }
---
+++ src/components/SplitButton/SplitButton.tsx
@@ -0,0 +1,102 @@
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import { type MouseEvent, useRef, useState } from 'react';
+
+interface Props {
+  options: string[];
+}
+
+export default function SplitButton(props: Props) {
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef<HTMLDivElement>(null);
+  const [selectedIndex, setSelectedIndex] = useState(1);
+
+  const handleClick = () => {
+    // console.info(`You clicked ${options[selectedIndex]}`);
+  };
+
+  const handleMenuItemClick = (
+    event: MouseEvent<HTMLLIElement>,
+    index: number,
+  ) => {
+    setSelectedIndex(index);
+    setOpen(false);
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: Event) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <ButtonGroup
+        variant="contained"
+        ref={anchorRef}
+        aria-label="split button"
+      >
+        <Button onClick={handleClick}>{props.options[selectedIndex]}</Button>
+        <Button
+          size="small"
+          aria-controls={open ? 'split-button-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          onClick={handleToggle}
+        >
+          <ArrowDropDownIcon />
+        </Button>
+      </ButtonGroup>
+
+      <Popper
+        sx={{ zIndex: 1 }}
+        open={open}
+        anchorEl={anchorRef.current}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === 'bottom' ? 'center top' : 'center bottom',
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu" autoFocusItem>
+                  {props.options.map((option, index) => (
+                    <MenuItem
+                      key={option}
+                      selected={index === selectedIndex}
+                      onClick={(event) => handleMenuItemClick(event, index)}
+                    >
+                      {option}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </>
+  );
+}
